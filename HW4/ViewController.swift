5ca2d97d5f88ee79da40fa6f27f2e34fa072c355
Dima Rusanov <roarisdour@gmail.com>
2017-09-05 18:19:55 +0300

    Initial Commit

---
+++ HW4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HW4
+//
+//  Created by Rusanov Dima on 05.09.17.
+//  Copyright © 2017 Rusanov Dima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
